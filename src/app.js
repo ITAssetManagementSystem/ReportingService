2108a30601ce10590f2223159e4d8e2ec82f7a41
Nilay Saxena <nilaysaxena94@gmail.com>
2025-12-22 07:18:05 +0000

    feat: add CORS support and update dependencies in package.json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const reportingRoutes = require("./routes/reporting.routes");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/health", (_, res) => res.send("OK"));
